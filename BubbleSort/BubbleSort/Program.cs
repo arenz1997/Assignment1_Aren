8a30860f9d43bc7c0b34bee69acbfdfb8027d3d1
Aren Gariki Zohranyan <arenz@rambler.ruz>
2019-03-30 03:39:10 +0400

    BubbleSort and Brackets
    Projects to review

---
+++ BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please input the size of array: ");
+            int size = int.Parse(Console.ReadLine()); 
+            int[] a = new int[size];
+            int i, j, k, max, min, average = 0;
+            Console.WriteLine("Please input the elements of array...");
+            for (i = 0; i < size; ++i) { a[i] = int.Parse(Console.ReadLine()); average += a[i]; }
+            max = min = a[0];
+
+            for (i = 0; i < size; ++i)
+            {
+                for (j = i; j < size; ++j)
+                {
+                    if (a[i] > a[j])
+                    {
+                        k = a[i];
+                        a[i] = a[j];
+                        a[j] = k;
+                    }
+                }
+            }
+            for (i = 0; i < size; ++i)
+            {
+                if (a[i] > max) max = a[i];
+                if (a[i] < min) min = a[i];
+            }
+            Console.Write("Sorted array: ");
+            for (i = 0; i < size; ++i)
+            {
+                Console.Write(a[i] + "\t");
+            }
+
+            Console.WriteLine("\nMin = " + min + ",\tMax = " + max + ",\tAverage = " + average/size+"\n");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pakagcer/Pakagcer/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pakagcer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            here:
+            Console.WriteLine("Hello! Please enter some brackets.");
+            StringBuilder s = new StringBuilder(Console.ReadLine());
+            if (s.Length == 0)
+            {
+                Console.Clear();
+                goto here;
+            }
+            for (int i = 0; i < s.Length; ++i)
+                if ((s[i] != '(') && (s[i] != ')') && (s[i] != '{') && (s[i] != '}') && (s[i] != '[') && (s[i] != ']'))
+                {
+                    Console.WriteLine("Please enter only brackets");
+                    Console.Clear();
+                    goto here;
+                }
+
+            if ((s.Length == 1) || (s[0] == ')') || (s[0] == '}') || (s[0] == ']'))
+            {
+                Console.WriteLine("NO");
+            }//for example: s is '(' or '{' or '['  OR starts with ')' or '}' or ']'
+            else
+            for (int j = 0; j < s.Length - 1; ++j)//  "((((())"  length = 7, jMax == 5
+            {
+                    if (
+                        ((s[j] == '(') && (s[j + 1] == '}')) || //   "...(}..."
+                        ((s[j] == '(') && (s[j + 1] == ']')) || //   "...(]..."
+                        ((s[j] == '{') && (s[j + 1] == ')')) || //   "...{)..."
+                        ((s[j] == '{') && (s[j + 1] == ']')) || //   "...{]..."
+                        ((s[j] == '[') && (s[j + 1] == ')')) || //   "...[)..."
+                        ((s[j] == '[') && (s[j + 1] == '}'))    //   "...[}..."
+                       )
+                    {
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                    if (((s[j] == '(') && (s[j + 1] == ')')) ||//   "...()..."
+                        ((s[j] == '{') && (s[j + 1] == '}')) ||//   "...{}..."
+                        ((s[j] == '[') && (s[j + 1] == ']')))  //   "...[]..."
+                    {
+                        s.Remove(j, 2); // "(({})" -> "(()"
+                        j = -1; //because in for loop j becomes 0 after ++j
+                        if (s.Length == 0)// no bracket
+                        {
+                            Console.WriteLine("YES");
+                            break;
+                        }
+                        if (s.Length == 1)// only one bracket
+                        {
+                            Console.WriteLine("NO");
+                            break;
+                        }
+                    } else if (j == s.Length - 2)// 
+                    {//   "(((" length=3, j=1. if the j indicates the last character and s[j] and s[j+1] stay in wrong order
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                }
+        Console.ReadKey();
+        }
+    }
+}
