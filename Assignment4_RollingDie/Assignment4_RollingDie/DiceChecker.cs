00e4cc85fd256bf316ebb2f1ce782302f205ff76
Aren Gariki Zohranyan <arenz@rambler.ruz>
2019-04-14 00:08:19 +0400

    Assignment4_DollingDie
    Project to review.

---
+++ Assignment4_RollingDie/Assignment4_RollingDie/DiceChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_RollingDie
+{
+    public class DiceEventArgs : EventArgs
+    {
+        public List<int> die_List { get; set; }
+        public int I { get; set; }
+    }
+    class DiceChecker
+    {
+        public delegate void CheckDiceCombination(object source, DiceEventArgs args);
+        public event CheckDiceCombination HasDouble4;
+        public event CheckDiceCombination HasSum20;
+
+        public List<int> dieList { get; set; }
+        public DiceChecker(List<int> dielist)
+        {
+            this.dieList = dielist;
+        }
+
+        public void Check_Double4()
+        {
+            for (int i = 0; i < dieList.Count - 1; ++i)
+            {
+                if ((dieList[i] == 4) && (dieList[i + 1] == 4))
+                {
+                    if (this.HasDouble4 != null)
+                    HasDouble4(this, new DiceEventArgs() { die_List = dieList, I = i });
+                }
+            }
+        }
+        public void Check_Sum20()
+        {
+            for (int i = 0; i < dieList.Count - 4; ++i)
+            {
+                if ((dieList[i] + dieList[i + 1] + dieList[i + 2] + dieList[i + 3] + dieList[i + 4]) >= 20)
+                {
+                    if (this.HasSum20 != null)
+                    HasSum20(this, new DiceEventArgs() { die_List = dieList, I = i});
+                }
+            }
+        }
+    }
+}
---
+++ Assignment4_RollingDie/Assignment4_RollingDie/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_RollingDie
+{
+    class Program
+    {
+        public static void Double4Handler(object source, DiceEventArgs e )
+        {
+            Console.WriteLine("two fours in a row");
+            doubleCount++;
+        }
+
+        public static void Sum20Handler(object source, DiceEventArgs e)
+        {
+            Console.Write(e.die_List[e.I].ToString() + "+" + e.die_List[e.I + 1].ToString() + "+" + e.die_List[e.I + 2].ToString() + "+" + e.die_List[e.I + 3].ToString() + "+" + e.die_List[e.I + 4].ToString() + "=");
+            Console.WriteLine(e.die_List[e.I]+e.die_List[e.I + 1]+e.die_List[e.I + 2]+e.die_List[e.I + 3]+e.die_List[e.I + 4]);
+            Console.WriteLine("Sum of 5 elements is greater than 20");
+        }
+        public static int doubleCount = 0;
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            List<int> diceList = new List<int>();
+            Random rnd = new Random();
+            for (int i = 0; i < 50; ++i)
+            {
+                diceList.Add(rnd.Next(1,6));
+                Console.Write(diceList[i]+" ");
+            }
+            Console.WriteLine();
+            DiceChecker diceChecker = new DiceChecker(diceList);
+            diceChecker.HasDouble4 += Double4Handler;
+            diceChecker.HasSum20 += Sum20Handler;
+            diceChecker.Check_Double4();
+            if (doubleCount != 0) Console.WriteLine(doubleCount + "x two fours\n");
+            diceChecker.Check_Sum20();
+        }
+    }
+}
